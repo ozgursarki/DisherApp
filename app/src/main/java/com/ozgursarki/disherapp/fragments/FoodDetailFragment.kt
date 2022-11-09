d924ca069568e2d70fb83315cb9ef3dbacc85c4e
ozgursarki <ozgurdogansarki@gmail.com>
2022-11-09 14:46:32 +0300

    Navigation component eklendi

---
+++ app/src/main/java/com/ozgursarki/disherapp/fragments/FoodDetailFragment.kt
@@ -0,0 +1,19 @@
+package com.ozgursarki.disherapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ozgursarki.disherapp.R
+
+
+class FoodDetailFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_food_detail, container, false)
+    }
+
+}
